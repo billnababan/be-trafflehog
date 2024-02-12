9a0c4babecaf01be6d94b6025ed80d041d99c753
billnababan <118668774+billnababan@users.noreply.github.com>
2024-02-12 10:46:31 -0500

    Update newDetect.js

--- controllers/newDetect.js
+++ controllers/newDetect.js
@@ -1,48 +1,51 @@
 const { exec } = require("child_process");
+const { log, error } = require("console");
+const { query } = require("../config/db");
 const fs = require("fs");
+const { stdout, stderr } = require("process");
 
-async function detectCredentials(url) {
+// async function detectCredentials(req, res) {
+//   const url = req.body;
+//   // console.log(url.url);
+//   try {
+//     const command = `trufflehog --regex --entropy False ${url.url}`;
+//     console.log(command);
+
+//     exec(command, (error, stdout, stderr) => {
+//       // console.log(stdout);
+//       return res.status(200).json({ data: stdout });
+//     });
+//   } catch (error) {
+//     return {
+//       url,
+//       error: error.message,
+//     };
+//   }
+// }
+
+async function detectCredentials(req, res) {
+  const url = req.body;
+  // console.log(url.url);
   try {
-    const command = `trufflehog ${url}`;
-    const { stdout, stderr } = await new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve({ stdout, stderr });
+    const command = `trufflehog --regex --entropy False ${url.url}`;
+    console.log(command);
+    exec(command, async (error, stdout, stderr) => {
+      // Validasi jika terdapat private key
+      // Simpan hasil scanning ke dalam database
+      const result = {
+        repo_url: url.url,
+        credential: stdout,
+        date: new Date(),
+      };
+      await query(
+        "INSERT INTO result_repo (repo_url, credential, date) VALUES (?, ?, ?)",
+        [result.repo_url, result.credential, result.date]
+      );
+      return res.status(200).json({
+        message: "Private key detected!",
+        data: result,
       });
     });
-
-    if (stderr) {
-      throw new Error(stderr);
-    }
-
-    const credentials = JSON.parse(stdout);
-
-    if (credentials && credentials.matches && credentials.matches.length > 0) {
-      const output = {
-        detectorType: "Trufflehog",
-        decoderType: "PLAIN",
-        rawResult: credentials,
-        line: 0,
-        timestamp: new Date().toISOString().slice(0, 19).replace("T", " "),
-        message: `Found credentials using Trufflehog`,
-      };
-
-      // Insert output into database
-      const date = new Date().toISOString().slice(0, 19).replace("T", " "); // Convert to MySQL datetime format
-      fs.appendFileSync("result_repo.txt", `${date} ${JSON.stringify(output)}\n`);
-
-      return output;
-    } else {
-      return {
-        data: {
-          url,
-          message: "No credentials found.",
-        },
-      };
-    }
   } catch (error) {
     return {
       url,
@@ -51,6 +54,26 @@ async function detectCredentials(url) {
   }
 }
 
+async function scanALlRepo(req, res) {
+  const git_url = req.body;
+  // console.log(url.url);
+  try {
+    const command = `trufflehog ${git_url.git_url}`;
+    console.log(command);
+
+    exec(command, (error, stdout, stderr) => {
+      // console.log(stdout);
+      return res.status(200).json({ data: stdout });
+    });
+  } catch (error) {
+    return {
+      git_url,
+      error: error.message,
+    };
+  }
+}
+
 module.exports = {
   detectCredentials,
+  scanALlRepo,
 };
