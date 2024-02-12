76d8ed95ed96bd1879d20f1cc409bcc353130766
billnababan <118668774+billnababan@users.noreply.github.com>
2024-02-12 10:48:25 -0500

    Update hasil.js

--- router-api/hasil.js
+++ router-api/hasil.js
@@ -1,19 +1,6 @@
 const Router = require("express").Router();
 const trufflehogController = require("../controllers/newDetect");
 
-Router.post("/deteksi", async (req, res) => {
-  const { url } = req.body;
-
-  if (!url) {
-    return res.status(400).json({ error: "URL is required." });
-  }
-
-  try {
-    const result = await trufflehogController.detectCredentials(url);
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
+Router.post("/deteksi", trufflehogController.detectCredentials);
+Router.post("/scan", trufflehogController.scanALlRepo);
 module.exports = Router;
